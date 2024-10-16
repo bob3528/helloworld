b32374bda031ede43629222b336012bfde4ee0b2
harre <harre@jose>
2024-10-16 09:36:48 +0800

    first commit

---
+++ helloworld/src/helloworld/helloworld1.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class helloworld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World I am JOE mama");
+		
+
+	}
+
+}
